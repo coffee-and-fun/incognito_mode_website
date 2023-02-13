faa4e67583025e65d919c9ab0d342e1431c623a9
RobertJGabriel <robert_gabriel@outlook.com>
2023-02-13 03:24:57 +0000

    Deploying to gh-pages from @ coffee-and-fun/incognito_mode_website@2d36770fcb463db0f8093fa9c4cde292e753fa87 🚀

---
+++ service-worker.js
@@ -0,0 +1,69 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("workbox-v4.3.1/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "workbox-v4.3.1"});
+
+workbox.core.setCacheNameDetails({prefix: "eleventy-plugin-pwa"});
+
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "assets/css/styles.css",
+    "revision": "d41d8cd98f00b204e9800998ecf8427e"
+  },
+  {
+    "url": "assets/images/favicon.ico",
+    "revision": "780996719e6ca94956a8551f1b87028e"
+  },
+  {
+    "url": "assets/images/logo.png",
+    "revision": "f0dd0e6d19c000c9fa7631e8cf9c278a"
+  },
+  {
+    "url": "assets/js/core/app.js",
+    "revision": "cd2389b4d55756792a625731341e1104"
+  },
+  {
+    "url": "assets/js/core/third-party.js",
+    "revision": "2c87c1baf103dde1c49b8729c36a94f4"
+  },
+  {
+    "url": "assets/js/index.js",
+    "revision": "0a30be565b1d1a6e649e77514f81a8d7"
+  },
+  {
+    "url": "assets/js/main.bundle.js",
+    "revision": "a78451e36d26dd567671b3075bfafe33"
+  },
+  {
+    "url": "assets/js/main.css",
+    "revision": "61f9734b35e757e22704e7b8569b00a2"
+  },
+  {
+    "url": "index.html",
+    "revision": "a950c7274c6d058985e991030640fa03"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/^.*\.(html|jpg|png|gif|webp|ico|svg|woff2|woff|eot|ttf|otf|ttc|json)$/, new workbox.strategies.StaleWhileRevalidate(), 'GET');
+workbox.routing.registerRoute(/^https?:\/\/fonts\.googleapis\.com\/css/, new workbox.strategies.StaleWhileRevalidate(), 'GET');
